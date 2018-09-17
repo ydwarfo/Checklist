71236cc66a8675a1b65e2fb54a63edb372ca8782
Olivier Béghain <olivier.beghain@icloud.com>
2018-09-17 20:57:56 +0200

    Initial Commit

---
+++ Checklist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Checklist
+//
+//  Created by Oli on 17/09/2018.
+//  Copyright © 2018 Smartouch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
